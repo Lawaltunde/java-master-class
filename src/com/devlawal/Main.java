609d70268d2ef52eb401828b6b78c331f2250ec5
Hammed Lawal <lawalolatunde14@gmail.com>
2026-03-14 16:42:50 -0400

    added User Pojo

--- src/com/devlawal/Main.java
+++ src/com/devlawal/Main.java
@@ -2,6 +2,22 @@ package com.devlawal;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        printOptions();
+    }
+
+    public static void printOptions() {
+        System.out.println(
+                """
+                1 - Book Car
+                2 - Delete Booking
+                3 - View All User Booked Cars
+                4 - View All Bookings
+                5 - View Available Cars
+                6 - View Available Electric Cars
+                7 - View All Users
+                8 - Exit
+                """
+        );
     }
 }
