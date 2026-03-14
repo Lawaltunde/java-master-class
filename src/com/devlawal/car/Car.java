afdcd7d7c8376f2faedcce771d3c234e87898374
Hammed Lawal <lawalolatunde14@gmail.com>
2026-03-14 16:47:04 -0400

    Car Pojo craeted with enum Brand

---
+++ src/com/devlawal/car/Car.java
@@ -0,0 +1,12 @@
+package com.devlawal.car;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class Car {
+    private UUID id;
+    private String regNumber;
+    private BigDecimal rentalPrice;
+    private Brand brand;
+    private boolean isElectric;
+}
