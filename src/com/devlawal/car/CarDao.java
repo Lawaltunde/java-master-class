8218efcf441bec04c7ab220dfaa658f072fab10c
Hammed Lawal <lawalolatunde14@gmail.com>
2026-03-19 18:50:04 -0400

    first implementation

--- src/com/devlawal/car/CarDao.java
+++ src/com/devlawal/car/CarDao.java
@@ -8,10 +8,10 @@ public class CarDao {
 
     static {
         cars = new Car[]{
-                new Car(UUID.fromString("2ea85178-fada-4279-9d5e-eea627049fa2"), "1234", new BigDecimal("100_000"), Brand.MERCEDES, true),
-                new Car(UUID.fromString("576590ff-57a1-4df3-8430-79980eb42343"), "5678", new BigDecimal("150_000"), Brand.TESLA, true),
-                new Car(UUID.fromString("9d818235-ce3b-40e8-b74a-3674985c6bcd"), "9012", new BigDecimal("90_000"), Brand.TOYOTA, false),
-                new Car(UUID.fromString("87cb62d9-d262-4174-b1b2-957f9e2a1f40"), "3456", new BigDecimal("60_000"), Brand.AUDI, false),
+                new Car(UUID.fromString("2ea85178-fada-4279-9d5e-eea627049fa2"), "1234", new BigDecimal("100"), Brand.MERCEDES, true),
+                new Car(UUID.fromString("576590ff-57a1-4df3-8430-79980eb42343"), "5678", new BigDecimal("150"), Brand.TESLA, true),
+                new Car(UUID.fromString("9d818235-ce3b-40e8-b74a-3674985c6bcd"), "9012", new BigDecimal("90"), Brand.TOYOTA, false),
+                new Car(UUID.fromString("87cb62d9-d262-4174-b1b2-957f9e2a1f40"), "3456", new BigDecimal("60"), Brand.AUDI, false),
         };
     }
 
