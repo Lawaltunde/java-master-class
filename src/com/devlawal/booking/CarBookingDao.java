a0b102d64da399684b289f2727f46b817caecac3
Hammed Lawal <lawalolatunde14@gmail.com>
2026-03-19 18:49:23 -0400

    fix(CarBookingDao.java):fixed logical errors in addBooking method

--- src/com/devlawal/booking/CarBookingDao.java
+++ src/com/devlawal/booking/CarBookingDao.java
@@ -4,34 +4,33 @@ public class CarBookingDao {
     private static CarBooking[] carBookings;
 
     static {
-        carBookings = new CarBooking[5];
+        carBookings = new CarBooking[0];
     }
 
     public CarBooking[] getAllBookings() {
         return carBookings;
     }
 
-    public boolean addBooking(CarBooking carBooking) {
-        int availableSlot = -1, index = -1;
-        for (CarBooking booking : getAllBookings()) {
-            index++;
-            if (booking == null) {
-                availableSlot = index;
-                break;
+    public void addBooking(CarBooking carBooking) {
+         int nextFreeSlot = -1;
+
+        for (int i = 0; i< carBookings.length; i++) {
+            if (carBookings[i] == null) {
+                nextFreeSlot = i;
             }
         }
-        if (availableSlot >= 0) {
-            carBookings[availableSlot] = carBooking;
-            return true;
+        if (nextFreeSlot > -1) {
+            carBookings[nextFreeSlot] = carBooking;
+            return;
         }
-        if (index >= 0) {
-            CarBooking[] newCarBookings = new CarBooking[carBookings.length + 5];
-            System.arraycopy(carBookings, 0, newCarBookings, 0, index);
-            newCarBookings[++index] = carBooking;
-            carBookings = newCarBookings;
-            return true;
+
+        CarBooking[] temp = new CarBooking[carBookings.length + 5];
+        for (int i = 0; i< carBookings.length; i++) {
+            temp[i] = carBookings[i];
         }
-        return false;
+        nextFreeSlot = carBookings.length;
+        temp[nextFreeSlot] = carBooking;
+        carBookings = temp;
     }
 
 }
