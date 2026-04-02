69f7a6fe2ad136c6c328edf6246743d4e695ef46
Hammed Lawal <lawalolatunde14@gmail.com>
2026-04-02 15:36:24 -0400

    added UserFileDataAccessService which implements UseDao

---
+++ src/com/devlawal/user/UserFileDataAccessService.java
@@ -0,0 +1,57 @@
+package com.devlawal.user;
+
+import java.io.*;
+import java.util.UUID;
+
+public class UserFileDataAccessService implements UserDao, Serializable {
+    private final String PATH = "src/com/devlawal/user/user.dat";
+    File file = new File(PATH);
+
+    // to seed user.dat with static data
+    static {
+        File file = new File("src/com/devlawal/user/user.dat");
+        if (!file.exists()) {
+            User[] users = new User[]{
+                    new User(UUID.fromString("8ca51d2b-aaaf-4bf2-834a-e02964e10fc3"), "Lawal"),
+                    new User(UUID.fromString("b10d126a-3608-4980-9f9c-aa179f5cebc3"), "James")
+
+            };
+            try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("src/com/devlawal/user/user.dat"))){
+                oos.writeObject(users);
+            }catch (IOException e){
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Override
+    public User[] getUsers() {
+        return getAllUsersFromFile();
+    }
+
+    @Override
+    public User getUserById(UUID id) {
+        User[] usersFromFile = getAllUsersFromFile();
+        if (usersFromFile == null || usersFromFile.length == 0) {
+            return null;
+        }
+        for (User user : usersFromFile) {
+            if (user.getId().equals(id)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    private User[] getAllUsersFromFile() {
+        if (!file.exists()) {
+            return new User[0];
+        }
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(PATH))) {
+            return (User[]) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
