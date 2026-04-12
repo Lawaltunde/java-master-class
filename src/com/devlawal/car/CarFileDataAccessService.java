38d87d37fbba55a515d3e16d188a1faf08616615
Hammed Lawal <lawalolatunde14@gmail.com>
2026-04-12 18:49:16 -0400

    car model list implementation

--- src/com/devlawal/car/CarFileDataAccessService.java
+++ src/com/devlawal/car/CarFileDataAccessService.java
@@ -2,6 +2,8 @@ package com.devlawal.car;
 
 import java.io.*;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class CarFileDataAccessService implements CarDao, Serializable {
@@ -13,14 +15,15 @@ public class CarFileDataAccessService implements CarDao, Serializable {
     static {
         File file = new File("src/com/devlawal/car/car.dat");
         if (!file.exists()) {
-            Car[] cars = new Car[]{
+            List<Car> cars = new ArrayList<>();
+            cars = List.of(
                     new Car(UUID.fromString("2ea85178-fada-4279-9d5e-eea627049fa2"), "1234", new BigDecimal("100"), Brand.MERCEDES, true),
                     new Car(UUID.fromString("576590ff-57a1-4df3-8430-79980eb42343"), "5678", new BigDecimal("150"), Brand.TESLA, true),
                     new Car(UUID.fromString("9d818235-ce3b-40e8-b74a-3674985c6bcd"), "9012", new BigDecimal("90"), Brand.TOYOTA, false),
-                    new Car(UUID.fromString("87cb62d9-d262-4174-b1b2-957f9e2a1f40"), "3456", new BigDecimal("60"), Brand.AUDI, false),
-            };
+                    new Car(UUID.fromString("87cb62d9-d262-4174-b1b2-957f9e2a1f40"), "3456", new BigDecimal("60"), Brand.AUDI, false)
+            );
 
-            try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("src/com/devlawal/car/car.dat"))) {
+            try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))) {
                 oos.writeObject(cars);
             } catch (IOException e) {
                 throw new RuntimeException(e);
@@ -29,14 +32,14 @@ public class CarFileDataAccessService implements CarDao, Serializable {
     }
 
     @Override
-    public Car[] getAllCars() {
+    public List<Car> getAllCars() {
         return getCarsFromFile();
     }
 
     @Override
     public Car getCarById(UUID id) {
-        Car[] cars = getCarsFromFile();
-        if (cars == null || cars.length == 0) {
+        List<Car> cars = getCarsFromFile();
+        if (cars == null || cars.isEmpty()) {
             return null;
         }
         for (Car car : getCarsFromFile()) {
@@ -47,12 +50,12 @@ public class CarFileDataAccessService implements CarDao, Serializable {
         return null;
     }
 
-    private Car[] getCarsFromFile() {
+    private List<Car> getCarsFromFile() {
         if (!file.exists()) {
-            return new Car[0];
+            return new ArrayList<>();
         }
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(PATH))) {
-            return (Car[]) ois.readObject();
+            return (List<Car>) ois.readObject();
         } catch (IOException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
